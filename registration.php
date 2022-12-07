45296eb03103ee66f61686d82ee42e7d6dafdadd
RIFANASHERIN <67692764+RIFANASHERIN@users.noreply.github.com>
2022-12-06 18:59:46 -0800

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,63 @@
+<?php
+require 'config.php';
+if(isset($_POST["submit"])){
+    $name=$_POST["name"];
+    $username=$_POST["username"];
+    $email=$_POST["email"];
+    $password=$_POST["password"];
+    $confirmpassword=$_POST["confirmpassword"];
+    $duplicate=mysqli_query($conn, "SELECT * FROM person WHERE username='$username' OR email='$email'");
+    if(mysqli_num_rows($duplicate)>0){
+        echo
+        "<script> alert('Username or Email Has Already Taken'); </script>";
+        }
+    else{
+        if($password==$confirmpassword){
+            $query="INSERT INTO person VALUES('','$name','$username','$email','$password')";
+            mysqli_query($conn,$query);
+            echo
+            "<script> alert('Registration Successful'); </script>";
+        }
+        else{
+            echo
+            "<script> alert('Password Does not Match'); </script>";
+
+        }
+        }
+    }
+
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <h1>Registration</h1>
+    <form class="" action="" method="post" autocomplete="off">
+        <label for="name">Name :</label>
+        <input type="text" name="name" id="name" required value=""><br>
+        <label for="username">Username :</label>
+        <input type="text" name="username" id="username" required value=""><br>
+        <label for="email">Email :</label>
+        <input type="text" name="email" id="email" required value=""><br>
+        <label for="password">Password :</label>
+        <input type="password" name="password" id="password" required value=""><br>
+        <label for="confirmpassword">Confirm Password :</label>
+        <input type="password" name="confirmpassword" id="confirmpassword" required value=""><br>
+        <button type="submit" name="submit">Register</button>
+    </form>
+    <br>
+    <a href="login.php">login</a>
+</body>
+</html>
